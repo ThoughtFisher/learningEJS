a567e2ae16e3eba430848fd8d4b568572a0cda45
root <root@ThoughtFisher.localdomain>
2020-03-18 17:49:11 -0500

    Loading the last changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // server.js
 // load the things we need
+//https://scotch.io/tutorials/use-ejs-to-template-your-node-application
 var express = require("express");
 var app = express();
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
-
+  
 // use res.render to load up an ejs view file
 
 // index page
